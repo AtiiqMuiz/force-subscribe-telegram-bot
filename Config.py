ef63c5184d759ecb606b702b76cec867d855fac0
muiz <53647688+AtiiqMuiz@users.noreply.github.com>
2021-02-24 23:32:52 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -11,10 +11,10 @@ class Config():
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
-    BOT_TOKEN = "1347401607:AAGRIdLkLUnMAW3aqaDGxQkuqfipJc9lzOY"
+    BOT_TOKEN = ""
     DATABASE_URL = ""
-    APP_ID = "2875927"
-    API_HASH = "c8e83de4c77a0de2527901bc77009282"
+    APP_ID = ""
+    API_HASH = ""
     SUDO_USERS = list(set(int(x) for x in ''.split()))
     SUDO_USERS.append(939425014)
     SUDO_USERS = list(set(SUDO_USERS))
